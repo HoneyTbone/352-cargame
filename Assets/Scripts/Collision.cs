893abfeeb70fd3304c13c853b5e62325d3da377b
Tyson Toller <honeytbone33@gmail.com>
2022-11-14 14:22:39 -0500

    Added Packages and delivery System

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    private bool hasPackage = false;
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("you hit " + other);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    public void OnTriggerEnter2D(Collider2D other) 
     {
         Debug.Log("you triggered " + other);
-        if(other.tag == "Box")
+        if(other.tag == "Box" && hasPackage==false)
         {
-            Debug.Log("You Lose");
+            Debug.Log("Picked up a Package");
+            hasPackage = true;
+            Destroy(other.gameObject);
+        }else if(other.tag == "Customer" && hasPackage==true)
+        {
+            Debug.Log("Delivered a Package");
+            hasPackage = false;
+        }else if(other.tag == "Box" && hasPackage==true)
+        {
+            Debug.Log("You already have a Package");
         }
     }
+    
 }
